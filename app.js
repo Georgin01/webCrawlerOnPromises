ea0ea75d7a0796b148fd8241bc6e534f9e9f867d
Galay Yuriy <y4109galay@gmail.com>
2020-02-11 21:54:54 +0200

    Little fix

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const ut = require('./utilities');
 const request = ut.promisify(require('request'));
+const cheerio = require('cheerio');
 
-let url = `https://www.marathonbet.ru/su/popular/Football?page=3&pageAction=getPage&_=${new Date().getTime()}`;
+let url = `https://www.marathonbet.ru/su/popular/Football?page=0&pageAction=getPage&_=${new Date().getTime()}`;
 
-function download(url) {
+function sendingReq(url) {
     console.log(`Requesting ${url}`);
     return request(url)
-        .then(response => response.body)
-        .then(content => JSON.parse(content))
-        .then(con => console.log(con[1].val))
-        .catch(err => console.log(err));
+        .then(response => JSON.parse(response.body))
+        .then(body => { return body; })
+        .catch(err => { if(err) throw err; });
 }
-
-download(url);
